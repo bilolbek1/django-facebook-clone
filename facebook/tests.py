03a020ddf5d23e1f78cad280c9e05e433d01e395
Bilolbek Abdumalikov <bilolabdumalikov1@gmail.com>
2024-02-18 20:09:29 +0500

    unit-test-3

--- facebook/tests.py
+++ facebook/tests.py
@@ -13,6 +13,7 @@ class FacebookTestCase(TestCase):
         self.user.save()
         self.client.login(username='test', password='test')
         Profile.objects.create(user=self.user)
+
     def test_home_page(self):
 
         Post.objects.create(user_id=self.user, title='test1', text='test1', image="media-files/post-media/crismum.jpg")
@@ -44,6 +45,17 @@ class FacebookTestCase(TestCase):
         user_followers_count = user.profile.followed.all().count()
         self.assertContains(response, user.image)
         self.assertContains(response, user_followers_count)
+
+
+    def test_users_search(self):
+        user1 = CustomUser.objects.create(username='test1', password='test1')
+        user2 = CustomUser.objects.create(username='test2', password='test2')
+
+        response = self.client.get(reverse('user-search')+"?q=test1")
+
+        self.assertContains(response, user1)
+        self.assertNotContains(response, user2)
+
 
 
 
